cb5be426147a4ea9f52e957495a20c889d717c18
Eenae <z-line@my.com>
2018-04-12 20:57:15 +0300

    more tests

--- test/SimpleVoting.js
+++ test/SimpleVoting.js
@@ -28,4 +28,75 @@ contract('SimpleVotingTest', function(accounts) {
         await expectThrow(voting.vote(100000000));
     });
 
+    it('test winnerId simple voting for 0', async function() {
+        const voting = await SimpleVoting.new();
+
+        await voting.vote(0);
+
+        assert.equal(await voting.winnerId(), 0);
+    });
+
+    it('test winnerId simple voting for 1', async function() {
+        const voting = await SimpleVoting.new();
+
+        await voting.vote(1);
+
+        assert.equal(await voting.winnerId(), 1);
+    });
+
+    it('test winnerId advanced voting for 1', async function() {
+        const voting = await SimpleVoting.new();
+
+        await voting.vote(1);
+        await voting.vote(0);
+        await voting.vote(1);
+
+        assert.equal(await voting.winnerId(), 1);
+    });
+
+    it('test winnerId advanced voting for 0', async function() {
+        const voting = await SimpleVoting.new();
+
+        await voting.vote(0);
+        await voting.vote(1);
+        await voting.vote(0);
+
+        assert.equal(await voting.winnerId(), 0);
+    });
+
+    it('test winnerId in case of a tie', async function() {
+        const voting = await SimpleVoting.new();
+
+        await voting.vote(2);
+        await voting.vote(0);
+        await voting.vote(1);
+        await voting.vote(0);
+        await voting.vote(1);
+
+        assert.equal(await voting.winnerId(), 0);
+    });
+
+    it('test winnerId in case of a tie #2', async function() {
+        const voting = await SimpleVoting.new();
+
+        await voting.vote(2);
+        await voting.vote(1);
+        await voting.vote(1);
+        await voting.vote(0);
+        await voting.vote(0);
+
+        assert.equal(await voting.winnerId(), 0);
+    });
+
+    it('test winnerId in case of a tie #3', async function() {
+        const voting = await SimpleVoting.new();
+
+        await voting.vote(2);
+        await voting.vote(0);
+        await voting.vote(0);
+        await voting.vote(1);
+        await voting.vote(1);
+
+        assert.equal(await voting.winnerId(), 0);
+    });
 });
